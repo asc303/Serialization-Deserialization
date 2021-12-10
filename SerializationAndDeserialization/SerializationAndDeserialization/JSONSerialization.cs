c4777f5e07cd82c7b362e16df0a6453f579e79a6
asc303 <chauhansam94@gmail.com>
2021-12-10 12:24:27 +0530

    [ashutosh] add:Serialization&Deserialization

---
+++ SerializationAndDeserialization/SerializationAndDeserialization/JSONSerialization.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SerializationAndDeserialization
+{
+    public class JSONSerialization
+    {
+        public void JsonSerialize()
+        {
+            BlogSites bsObj = new BlogSites()
+            {
+                Name = "Ashutosh",
+                Description = "Welcome to the Serialization"
+            };
+            string jsonData = JsonConvert.SerializeObject(bsObj);
+            Console.WriteLine(jsonData);
+            Console.WriteLine();
+        }
+        public void JsonDeserialize()
+        {
+            string json = @"{
+                    'Name' : 'Ashutosh',
+                    'Description' : 'Welcome to the Serialization'
+            }";
+            BlogSites bsObj = JsonConvert.DeserializeObject<BlogSites>(json);
+            Console.WriteLine(bsObj.Name);
+            Console.WriteLine(bsObj.Description);
+            Console.WriteLine();
+        }
+    }
+    [DataContract]
+    public class BlogSites
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+    }
+}
